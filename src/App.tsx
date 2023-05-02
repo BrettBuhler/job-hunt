003e7183eb34ae96a85702b812cca3c30c0b454e
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-05-02 16:32:53 -0600

    Google Oauth Login
    Implemented Google Oauth authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage'
+import { googleLogout, useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
+
 
 const App = () => {
-  const [userName, setUserName] = useState('guest')
+  const [userName, setUserName] = useState<string>('')
+  const [user, setUser] = useState<object>({})
+  const [profile, setProfile] = useState<object>({})
+
   return (
-    <LandingPage setUserName={setUserName}/>
+    <LandingPage setUserName={setUserName} user={user} setUser={setUser} profile={profile} setProfile = {setProfile} userName={userName}/>
   );
 }
 
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,17 +1,25 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {GoogleLoginButton, FacebookLoginButton, GithubLoginButton} from 'react-social-login-buttons'
+import {googleLogout, useGoogleLogin} from '@react-oauth/google'
+import axios from 'axios'
 
 /**
- * The setUserName function is passed to the LandingPage component. Once users have loged in,
- * setUserName is called with the appropriate google/facebook/github login
+ * A landing page component that allows users to log in or sign up using their email, 
+ * Google, Facebook, or Github accounts. Once logged in, the user's profile information 
+ * is retrieved using the Google OAuth API and displayed on the page. The user can also 
+ * log out, which clears their profile information and sets the user name to an empty string.
+ * 
+ * TODO: Implement facebook and Github login
  */
 interface LandingPageProps {
-    setUserName: (name: string) => void
+    userName: string,
+    setUserName: (name: string) => void,
+    user: object,
+    setUser: (aUser: object) => void,
+    profile: object,
+    setProfile: (aProfile: object) => void
 }
 
-const handleGoogleLogin = () => {
-    console.log('google')
-}
 
 const handleFacebookLogin = () => {
     console.log('facebook')
@@ -21,13 +29,51 @@ const handleGithubLogin = () => {
     console.log('github')
 }
 
-const handleEmailSignup = (event: React.FormEvent) => {
-    console.log(event)
-}
 
-const LandingPage: React.FC<LandingPageProps> = () => {
+const LandingPage: React.FC<LandingPageProps> = ({ setUser, setProfile, user, profile , setUserName, userName}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+
+    /**
+     * ToDo: Check database for user email, if it exists, check if passowrds match, if they do LOGIN, if they don't PASSWORD ERROR, if the email
+     * does not exist, take user to confiermantion screen + make account
+     * @param event Submit event
+     */
+    const handleEmailSignup = (event: React.FormEvent) => {
+        event.preventDefault()
+        setUserName(email)
+    }
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => {
+            setUser(codeResponse)
+        },
+        onError: (error) => console.log('Login Failed:', error)
+      })
+    
+      const logOut = () => {
+        googleLogout()
+        setProfile({})
+        setUserName('')
+      }
+    
+      useEffect(()=>{
+        if ((user as any).access_token) {
+          axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${(user as any).access_token}`,{
+            headers: {
+              Authorization: `Bearer ${(user as any).access_token}`,
+              Accept: 'application/json'
+            }
+          })
+          .then((res:object) => {
+            setProfile((res as any).data)
+            setUserName((res as any).data.email)
+          })
+          .catch((err:object) => console.log(err))
+        }
+      },[ user ])
+
 
     return (
         <div className="landingPage">
@@ -39,9 +85,11 @@ const LandingPage: React.FC<LandingPageProps> = () => {
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' autoComplete='current-password'></input>
                     <input type="submit" value={'LogIn / Create'}></input>
                 </form>
-                <GoogleLoginButton onClick={handleGoogleLogin}/>
+                <GoogleLoginButton onClick={() => login()}/>
                 <FacebookLoginButton onClick={handleFacebookLogin}/>
                 <GithubLoginButton onClick={handleGithubLogin}/>
+                <button onClick={()=>console.log((profile as any), userName)}></button>
+                {userName !== '' ? <button onClick={logOut}>LOG OUT</button> : <div></div>}
             </div>
         </div>
     )
